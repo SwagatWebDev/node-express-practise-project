e6164e1ad160cde078d539b116d0f28b24d3faf6
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-12 12:02:39 +0530

    File updated

--- test.js
+++
@@ -1,9 +0,0 @@
-const customEncode = str => encodeURIComponent(str).replace(/[()]/g, encodeURIComponent);
-
-// Example usage:
-const originalString1 = "Universal SSH (UKM)";
-const originalString2 = "Azure(GTK)";
-const encodedString1 = customEncode(originalString1);
-const encodedString2 = customEncode(originalString2);
-console.log(encodedString1); // Output: "Universal%20SSH%20%28UKM%29"
-console.log(encodedString2); // Output: "Azure%28GTK%29"
