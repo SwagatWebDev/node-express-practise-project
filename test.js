f9e1d211420ed8e3767edc25ea271c27c2e923dd
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-12 11:53:37 +0530

    File updated

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-const customEncode = str => encodeURIComponent(str).replace(/[()]/g, c => '%' + c.charCodeAt(0).toString(16).toUpperCase());
-
-// Example usage:
-const originalString = "Azure(ACR)";
-const encodedString = customEncode(originalString);
-console.log(encodedString); // Output: "Azure%28ACR%29"
+const customEncode = str => encodeURIComponent(str).replace(/[()]/g, c => encodeURIComponent(c));
