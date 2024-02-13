23285bf306b4104915f19d4924d807e446fc145f
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-13 15:33:17 +0530

    file updated

--- test.js
+++
@@ -1,6 +0,0 @@
-function noWhitespaceValidator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-        const isWhitespace = (control.value || '').trim().length === 0;
-        return isWhitespace ? { 'whitespace': true } : null;
-    };
-}
