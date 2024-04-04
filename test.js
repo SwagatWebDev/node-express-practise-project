ec28bef277348052b69b565ed245bb290f391a5e
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-04-05 02:30:39 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,40 @@
 ORDER BY
     s.is_core_common DESC,
-    CASE s.is_core_common
-        WHEN true THEN
-            FIELD(r.usage_disposition, 'Required', 'Preferred', 'Restricted', 'Emerging', 'Decommission', 'Prohibited'),
-            FIELD(s.reuse_model, 'Shared Platform', 'Shared System', 'Shared Code', 'Shared Component'),
+    CASE
+        WHEN s.is_core_common = true THEN
+            CASE
+                WHEN r.usage_disposition = 'Required' THEN 1
+                WHEN r.usage_disposition = 'Preferred' THEN 2
+                WHEN r.usage_disposition = 'Restricted' THEN 3
+                WHEN r.usage_disposition = 'Emerging' THEN 4
+                WHEN r.usage_disposition = 'Decommission' THEN 5
+                WHEN r.usage_disposition = 'Prohibited' THEN 6
+                ELSE 7
+            END,
+            CASE
+                WHEN s.reuse_model = 'Shared Platform' THEN 1
+                WHEN s.reuse_model = 'Shared System' THEN 2
+                WHEN s.reuse_model = 'Shared Code' THEN 3
+                WHEN s.reuse_model = 'Shared Component' THEN 4
+                ELSE 5
+            END,
             s.name
         ELSE
-            FIELD(r.usage_disposition, 'Required', 'Preferred', 'Restricted', 'Emerging', 'Decommission', 'Prohibited'),
-            FIELD(s.reuse_model, 'Shared Platform', 'Shared System', 'Shared Code', 'Shared Component'),
+            CASE
+                WHEN r.usage_disposition = 'Required' THEN 1
+                WHEN r.usage_disposition = 'Preferred' THEN 2
+                WHEN r.usage_disposition = 'Restricted' THEN 3
+                WHEN r.usage_disposition = 'Emerging' THEN 4
+                WHEN r.usage_disposition = 'Decommission' THEN 5
+                WHEN r.usage_disposition = 'Prohibited' THEN 6
+                ELSE 7
+            END,
+            CASE
+                WHEN s.reuse_model = 'Shared Platform' THEN 1
+                WHEN s.reuse_model = 'Shared System' THEN 2
+                WHEN s.reuse_model = 'Shared Code' THEN 3
+                WHEN s.reuse_model = 'Shared Component' THEN 4
+                ELSE 5
+            END,
             s.name
     END;
