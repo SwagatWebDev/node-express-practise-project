5b780213a0b1380c390dcbdbd04eb2122b2e3d8f
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-04-05 02:28:27 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 ORDER BY
     s.is_core_common DESC,
-    CASE WHEN s.is_core_common THEN
-        FIND_IN_SET(r.usage_disposition, 'Required,Preferred,Restricted,Emerging,Decommission,Prohibited'),
-        FIND_IN_SET(s.reuse_model, 'Shared Platform,Shared System,Shared Code,Shared Component'),
-        s.name
-    END,
-    r.usage_disposition,
-    s.reuse_model,
-    s.name;
+    CASE s.is_core_common
+        WHEN true THEN
+            FIELD(r.usage_disposition, 'Required', 'Preferred', 'Restricted', 'Emerging', 'Decommission', 'Prohibited'),
+            FIELD(s.reuse_model, 'Shared Platform', 'Shared System', 'Shared Code', 'Shared Component'),
+            s.name
+        ELSE
+            FIELD(r.usage_disposition, 'Required', 'Preferred', 'Restricted', 'Emerging', 'Decommission', 'Prohibited'),
+            FIELD(s.reuse_model, 'Shared Platform', 'Shared System', 'Shared Code', 'Shared Component'),
+            s.name
+    END;
