726e97d75ede5dc0928384b9f8dd3e164184255b
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-04-05 02:52:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 ORDER BY
-    CASE r.usage_disposition
-        WHEN 'Required' THEN 1
-        WHEN 'Preferred' THEN 2
-        WHEN 'Restricted' THEN 3
-        WHEN 'Emerging' THEN 4
-        WHEN 'Decommission' THEN 5
-        WHEN 'Prohibited' THEN 6
+    CASE
+        WHEN r.usage_disposition = 'Required' THEN 1
+        WHEN r.usage_disposition = 'Preferred' THEN 2
+        WHEN r.usage_disposition = 'Restricted' THEN 3
+        WHEN r.usage_disposition = 'Emerging' THEN 4
+        WHEN r.usage_disposition = 'Decommission' THEN 5
+        WHEN r.usage_disposition = 'Prohibited' THEN 6
         ELSE 7
     END,
-    LEAST(
-        CASE s.reuse_model WHEN 'Shared Platform' THEN 1
-                            WHEN 'Shared System' THEN 2
-                            WHEN 'Shared Code' THEN 3
-                            WHEN 'Shared Component' THEN 4
-                            ELSE 5 END
-    ),
+    CASE
+        WHEN s.reuse_model = 'Shared Platform' THEN 1
+        WHEN s.reuse_model = 'Shared System' THEN 2
+        WHEN s.reuse_model = 'Shared Code' THEN 3
+        WHEN s.reuse_model = 'Shared Component' THEN 4
+        ELSE 5
+    END,
     s.name;
