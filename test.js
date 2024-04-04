c6a4c02528cfd5b1ac1bba146d1ce1049c6600a9
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-04-05 02:33:58 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,40 +1,17 @@
 ORDER BY
-    s.is_core_common DESC,
     CASE
-        WHEN s.is_core_common = true THEN
-            CASE
-                WHEN r.usage_disposition = 'Required' THEN 1
-                WHEN r.usage_disposition = 'Preferred' THEN 2
-                WHEN r.usage_disposition = 'Restricted' THEN 3
-                WHEN r.usage_disposition = 'Emerging' THEN 4
-                WHEN r.usage_disposition = 'Decommission' THEN 5
-                WHEN r.usage_disposition = 'Prohibited' THEN 6
-                ELSE 7
-            END,
-            CASE
-                WHEN s.reuse_model = 'Shared Platform' THEN 1
-                WHEN s.reuse_model = 'Shared System' THEN 2
-                WHEN s.reuse_model = 'Shared Code' THEN 3
-                WHEN s.reuse_model = 'Shared Component' THEN 4
-                ELSE 5
-            END,
-            s.name
-        ELSE
-            CASE
-                WHEN r.usage_disposition = 'Required' THEN 1
-                WHEN r.usage_disposition = 'Preferred' THEN 2
-                WHEN r.usage_disposition = 'Restricted' THEN 3
-                WHEN r.usage_disposition = 'Emerging' THEN 4
-                WHEN r.usage_disposition = 'Decommission' THEN 5
-                WHEN r.usage_disposition = 'Prohibited' THEN 6
-                ELSE 7
-            END,
-            CASE
-                WHEN s.reuse_model = 'Shared Platform' THEN 1
-                WHEN s.reuse_model = 'Shared System' THEN 2
-                WHEN s.reuse_model = 'Shared Code' THEN 3
-                WHEN s.reuse_model = 'Shared Component' THEN 4
-                ELSE 5
-            END,
-            s.name
+        WHEN r.usage_disposition = 'Required' THEN 1
+        WHEN r.usage_disposition = 'Preferred' THEN 2
+        WHEN r.usage_disposition = 'Restricted' THEN 3
+        WHEN r.usage_disposition = 'Emerging' THEN 4
+        WHEN r.usage_disposition = 'Decommission' THEN 5
+        WHEN r.usage_disposition = 'Prohibited' THEN 6
+        ELSE 7 -- This covers any other values that might appear
+    END,
+    CASE s.reuse_model
+        WHEN 'Shared Platform' THEN 1
+        WHEN 'Shared System' THEN 2
+        WHEN 'Shared Code' THEN 3
+        WHEN 'Shared Component' THEN 4
+        ELSE 5 -- This covers any other values that might appear
     END;
