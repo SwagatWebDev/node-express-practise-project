cf24394e9483880a764047114e79d2b925d59d0e
Swagat Mishra <swagatkumarmishra888@gmail.com>
2024-08-14 03:00:19 +0530

    Update users.js

--- model/users.js
+++ model/users.js
@@ -6,3 +6,24 @@ const userSchema = new mongoose.Schema({
 });
 
 exports.User = mongoose.model('User', userSchema);
+
+
+String generateXpathFromElement(WebElement element) {
+    String tagName = element.getTagName()
+    String className = element.getAttribute('class')
+    String textContent = element.getText()
+    
+    String xpath = "//" + tagName
+
+    // Add class attribute if present
+    if (className != null && !className.isEmpty()) {
+        xpath += "[@class='" + className + "']"
+    }
+
+    // Add text content if present
+    if (textContent != null && !textContent.isEmpty()) {
+        xpath += "[text()='" + textContent + "']"
+    }
+
+    return xpath
+}
